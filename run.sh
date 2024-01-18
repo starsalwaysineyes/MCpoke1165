1db0338c6b7651d95111c0d0472637f12517ad14
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2024-01-18 11:08:32 +0000

    create world

---
+++ run.sh
@@ -0,0 +1,6 @@
+#sudo apt update
+#sudo apt-get install openjdk-8-jre-headless
+
+#npm i ngrok --save-dev
+
+/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -jar -Xms1024m -Xmx6144m /workspaces/MCpoke1165/forge-1.16.5-36.2.34.jar
